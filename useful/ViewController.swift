4b7e542ef9c405929b4e695be8f860eca15a2421
Mykhailo Herasimov <mogerasimov@gmail.com>
2020-04-26 18:26:51 -0300

    added structure

---
+++ useful/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  useful
+//
+//  Created by Mykhailo Herasimov on 2020-04-26.
+//  Copyright © 2020 Mykhailo Herasimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
