ee5ecaac4a846e5909d184cce283b14b355c107e
Mykhailo Herasimov <mogerasimov@gmail.com>
2023-03-03 01:20:19 -0500

    reconfigured plugins to run from root

--- packages/SourceCode/Package.swift
+++ packages/SourceCode/Package.swift
@@ -4,47 +4,47 @@
 import PackageDescription
 
 let package = Package(
-  name: "sourceCode",
-  platforms: [.macOS(.v10_13)],
-  products: [
-    .plugin(name: "usefulFormat", targets: ["usefulFormat"]),
-    //    .executable(name: "sourceCode", targets: ["CodeFormatterTool"])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
-  ],
-  targets: [
-    .plugin(
-      name: "usefulFormat",
-      capability: .command(
-        intent: .custom(
-          verb: "format",
-          description: "Formats Swift source files according to the Airbnb Swift Style Guide"),
-        permissions: [
-          .writeToPackageDirectory(reason: "Format Swift source files"),
-        ]),
-      dependencies: [
-        "CodeFormatterTool",
-        "SwiftFormat",
-        "SwiftLintBinary",
-      ]),
-    .executableTarget(
-      name: "CodeFormatterTool",
-      dependencies: [
-        .product(name: "ArgumentParser", package: "swift-argument-parser"),
-      ],
-      //              path: "packages/CodeFormatterTool",
-      //              sources: ["main.swift"],
-      resources: [
-        .process("useful.swiftformat"),
-        .process("swiftlint.yml"),
-      ]),
-    .binaryTarget(
-      name: "SwiftFormat",
-      url: "https://github.com/calda/SwiftFormat/releases/download/0.51-beta-6/SwiftFormat.artifactbundle.zip",
-      checksum: "8583456d892c99f970787b4ed756a7e0c83a0d9645e923bb4dae10d581c59bc3"),
-    .binaryTarget(
-      name: "SwiftLintBinary",
-      url: "https://github.com/realm/SwiftLint/releases/download/0.48.0/SwiftLintBinary-macos.artifactbundle.zip",
-      checksum: "9c255e797260054296f9e4e4cd7e1339a15093d75f7c4227b9568d63edddba50"),
-  ])
+    name: "sourceCode",
+    platforms: [.macOS(.v10_13)],
+    products: [
+        .plugin(name: "usefulFormat", targets: ["usefulFormat"]),
+        //    .executable(name: "sourceCode", targets: ["CodeFormatterTool"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
+    ],
+    targets: [
+        .plugin(
+            name: "usefulFormat",
+            capability: .command(
+                intent: .custom(
+                    verb: "format",
+                    description: "Formats Swift source files according to the Airbnb Swift Style Guide"),
+                permissions: [
+                    .writeToPackageDirectory(reason: "Format Swift source files"),
+                ]),
+            dependencies: [
+                "CodeFormatterTool",
+                "SwiftFormat",
+                "SwiftLintBinary",
+            ]),
+        .executableTarget(
+            name: "CodeFormatterTool",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ],
+            //              path: "packages/CodeFormatterTool",
+            //              sources: ["main.swift"],
+            resources: [
+                .process("useful.swiftformat"),
+                .process("swiftlint.yml"),
+            ]),
+        .binaryTarget(
+            name: "SwiftFormat",
+            url: "https://github.com/calda/SwiftFormat/releases/download/0.51-beta-6/SwiftFormat.artifactbundle.zip",
+            checksum: "8583456d892c99f970787b4ed756a7e0c83a0d9645e923bb4dae10d581c59bc3"),
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.48.0/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "9c255e797260054296f9e4e4cd7e1339a15093d75f7c4227b9568d63edddba50"),
+    ])
--- packages/SourceCode/README.md
+++ packages/SourceCode/README.md
@@ -1,3 +1,8 @@
 # sourceCode
 
 A description of this package.
+
+swift package --package-path packages/SourceCode --allow-writing-to-package-directory --disable-sandbox plugin format useful
+
+
+swift package --package-path packages/SourceCode --disable-sandbox format useful
